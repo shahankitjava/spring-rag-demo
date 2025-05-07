6e4781e1d533f84979cd6d02221aa98db5208cce
shahankitjava <shahankit.java@gmail.com>
2025-05-06 21:57:35 -0500

    Initial commit

---
+++ src/main/java/org/ankit/demo/spring_rag_demo/SpringRagDemoApplication.java
@@ -0,0 +1,13 @@
+package org.ankit.demo.spring_rag_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRagDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRagDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ankit/demo/spring_rag_demo/SpringRagDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ankit.demo.spring_rag_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRagDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
